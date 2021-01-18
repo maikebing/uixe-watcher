f770a9172dbc839f3ff4a17939c8606977f90197
MysticBoy <mysticboy@live.com>
2021-01-18 20:52:53 +0800

    发布地址

--- Uixe.Watcher/Properties/AssemblyInfo.cs
+++ Uixe.Watcher/Properties/AssemblyInfo.cs
@@ -6,6 +6,10 @@ using System.Runtime.InteropServices;
 // 控制。更改这些属性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("新e畅")]
-[assembly: AssemblyDescription("监控车道收费状态")]
+[assembly: AssemblyDescription("车道监控软件")]
 [assembly: ComVisible(false)]
 [assembly: Guid("67bc2c15-4f30-4742-b812-95dbd6a4071c")]
+[assembly: AssemblyCompany("新疆交投科技有限责任公司")]
+[assembly: AssemblyProduct("新e畅")]
+[assembly: AssemblyCopyright("Copyright 新疆交投科技有限责任公司")]
+[assembly: AssemblyTrademark("新e畅")]
