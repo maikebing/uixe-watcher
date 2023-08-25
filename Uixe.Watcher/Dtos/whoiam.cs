70c873711cf91cde188a3ecf4819897a5526c9fa
MysticBoy <mysticboy@live.com>
2023-08-25 17:45:26 +0800

    处理警告。

--- Uixe.Watcher/Dtos/whoiam.cs
+++ Uixe.Watcher/Dtos/whoiam.cs
@@ -2,7 +2,9 @@
 
 namespace Uixe.Watcher.Dtos
 {
+#pragma warning disable CS8981 // 该类型名称仅包含小写 ascii 字符。此类名称可能会成为该语言的保留值。
     public class whoiam
+#pragma warning restore CS8981 // 该类型名称仅包含小写 ascii 字符。此类名称可能会成为该语言的保留值。
     {
         /// <summary>
         /// 自治区总中心1
