e231deaee716a4e8f54a642eb2dd947818648408
MysticBoy <mysticboy@live.com>
2024-08-22 13:41:18 +0800

    加入节约化 站类型说明

--- Uixe.Watcher/Dtos/whoiam.cs
+++ Uixe.Watcher/Dtos/whoiam.cs
@@ -2,6 +2,26 @@
 
 namespace Uixe.Watcher.Dtos
 {
+    public enum PlazaType
+    {
+        None = 0,
+        /// <summary>
+        /// 普通站
+        /// </summary>
+        Plaza ,
+        /// <summary>
+        /// 分公司的中心
+        /// </summary>
+        BranchCenter, 
+        /// <summary>
+        /// 省中心
+        /// </summary>
+        ProvCenter,
+        /// <summary>
+        /// 集约化主管收费站
+        /// </summary>
+        Supervisor 
+    }
 #pragma warning disable CS8981 // 该类型名称仅包含小写 ascii 字符。此类名称可能会成为该语言的保留值。
     public class whoiam
 #pragma warning restore CS8981 // 该类型名称仅包含小写 ascii 字符。此类名称可能会成为该语言的保留值。
@@ -11,12 +31,10 @@ namespace Uixe.Watcher.Dtos
         /// </summary>
         public string name { get; set; }
         /// <summary>
-        /// 
+        /// 站类型 
         /// </summary>
-        public string agentIp { get; set; }
-        /// <summary>
-        /// 
-        /// </summary>
+        public PlazaType plazaType { get; set; } = PlazaType.None;
+ 
         public List<Plaza> plazas { get; set; }
     }
 }
