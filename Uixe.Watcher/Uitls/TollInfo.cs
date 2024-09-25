b280e0bea5b06a42ac5fccac57edd5883fd9f32c
MysticBoy <mysticboy@live.com>
2024-09-25 15:27:25 +0800

    修改

--- Uixe.Watcher/Uitls/TollInfo.cs
+++ Uixe.Watcher/Uitls/TollInfo.cs
@@ -8,31 +8,42 @@ namespace Uixe.Watcher.Uitls
 {
     public static class TollInfo
     {
-        public static async Task<Plaza> GetTollInfo(string toll_id, bool reset = false)
-        {
-            Plaza plaza1 = null;
-            var client = new RestClient("http://10.165.70.45:5000/").AddDefaultHeader(KnownHeaders.Accept, "*/*");
-            var request = new RestRequest("/get_toll_info", Method.Post).AddHeader("Content-Type", "application/json").AddJsonBody(new { toll_id });
-            var response = await client.ExecutePostAsync(request);
-            if (response.StatusCode == System.Net.HttpStatusCode.OK)
-            {
-                Debug.WriteLine(response.Content);
-                plaza1 = Newtonsoft.Json.JsonConvert.DeserializeObject<Plaza>(response.Content);
-            }
-            return plaza1;
-        }
+
+ 
+        private const string BaseUrl = "http://10.165.70.45:5000/";
+     
         public static async Task<whoiam> Guesswhoiam()
         {
             whoiam whoiam = null;
-             var client = new RestClient("http://10.165.70.45:5000/").AddDefaultHeader(KnownHeaders.Accept, "*/*");
+            var client = new RestClient(BaseUrl).AddDefaultHeader(KnownHeaders.Accept, "*/*");
             var request = new RestRequest("/guesswhoiam", Method.Post).AddHeader("Content-Type", "application/json");
             var response = await client.ExecutePostAsync(request);
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 Debug.WriteLine(response.Content);
                 whoiam = Newtonsoft.Json.JsonConvert.DeserializeObject<whoiam>(response.Content);
+                await upload_boss(response.Content);
             }
             return whoiam;
         }
+
+        public static async Task upload_boss(string json)
+        {
+            try
+            {
+                using var client = new RestClient("http://10.165.84.44/").AddDefaultHeader(KnownHeaders.Accept, "*/*");
+                var request = new RestRequest("/api/Bosses/upload_boss", Method.Post).AddHeader("Content-Type", "application/json");
+                request.AddStringBody(json, DataFormat.Json);
+                var response = await client.ExecutePostAsync(request);
+                Debug.WriteLine(response.Content);
+            }
+            catch (Exception)
+            {
+
+      
+            }
+           
+        }
+
     }
 }
